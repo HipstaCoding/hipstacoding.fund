dc100c8571620bb8486ab19b9d4b5049d14baed9
Alexey Smirnov <xlwknx@gmail.com>
2022-09-23 22:40:51 +0200

    fix requisites and download stories

--- public/js/requisites.js
+++ public/js/requisites.js
@@ -1,6 +1,7 @@
 const clipboard = new ClipboardJS('label');
-
+console.log('clipboard', clipboard)
 clipboard.on('success', function(e) {
+
   tippy(e.trigger.querySelector('.fa-copy'), {
     content: 'copied!',
     showOnCreate: true,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+
+router.get('/instagram/', function(req, res, next) {
+  console.log('GET INSTAGRAM', __dirname)
+  res.sendFile('./view/instagram/index.html', { root: __dirname }, function (err) {
+    if (err) {
+      console.log('GET error', err, err.message)
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  });
 });
 
 module.exports = router;
