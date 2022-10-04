5fa333592b1eaa865eccbff34fee0792a0028b39
Alexey Smirnov <xlwknx@gmail.com>
2022-10-04 23:10:31 +0200

    remove unused css

--- features/social-sharing/instagram-story.js
+++ features/social-sharing/instagram-story.js
@@ -5,29 +5,30 @@ const font2base64 = require("node-font2base64");
 const formatMonobankData = require("../donation-tracker/formatMonobankData");
 const { imageFromBufferToBase64 } = require("../../utils/toBase64");
 
+const FONTS_CSS = "public/styles/fonts.css";
 const INSTA_STORY_TEMPLATE_HTML = "views/social-sharing/story.hbs";
-const COMPONENTS_CSS = "views/social-sharing/components.css";
 const STORY_CSS = "views/social-sharing/story.css";
-const VARIABLES_CSS = "views/social-sharing/variables.css";
-const OVERRIDES_CSS = "views/social-sharing/overrides.css";
-const HERO_IMG = "views/images/hero.jpg";
+const COMPONENTS_CSS = "public/styles/components.css";
+const VARIABLES_CSS = "public/styles/variables.css";
+const OVERRIDES_CSS = "public/styles/overrides.css";
+const HERO_IMG = "public/images/hero.jpg";
 
-const CSS_FILES = [VARIABLES_CSS, OVERRIDES_CSS, COMPONENTS_CSS, STORY_CSS];
+const CSS_FILES = [FONTS_CSS, VARIABLES_CSS, OVERRIDES_CSS, COMPONENTS_CSS, STORY_CSS];
 
 // TELL ABOUT THIS FUNCTION WHY WE NEED IT
 const getPageTemplate = (pagePath) => fs.readFile(pagePath, { encoding: "utf-8" });
 
 const getInstaStoryParams = async () => {
   console.log("getInstaStoryParams 0");
-  const font = await font2base64.encodeToDataUrl("views/fonts/fonts.ttf");
+  const font = await font2base64.encodeToDataUrl("public/fonts/fonts.ttf");
   console.log("getInstaStoryParams 1");
+  const cssFilesContent = CSS_FILES.map((path) => fs.readFile(path, { encoding: "utf-8" }));
   const [imageContent, ...cssPages] = await Promise.all(
-    [fs.readFile(HERO_IMG)].concat(
-      CSS_FILES.map((path) => fs.readFile(path, { encoding: "utf-8" }))
-    )
+    [fs.readFile(HERO_IMG)].concat(cssFilesContent)
   );
   console.log("getInstaStoryParams 2");
   const css = cssPages.join("\n");
+  console.log("cssFilesContent", css);
   const heroImg = imageFromBufferToBase64(imageContent);
 
   return {
@@ -38,11 +39,7 @@ const getInstaStoryParams = async () => {
 };
 
 async function createInstagramStoryImage(jar) {
-
-  const promises = [
-    getInstaStoryParams(),
-    getPageTemplate(INSTA_STORY_TEMPLATE_HTML),
-  ];
+  const promises = [getInstaStoryParams(), getPageTemplate(INSTA_STORY_TEMPLATE_HTML)];
   const [{ font, css, heroImg }, page] = await Promise.all(promises);
 
   const data = formatMonobankData(jar);
--- features/social-sharing/twitter-post.js
+++ features/social-sharing/twitter-post.js
@@ -5,20 +5,22 @@ const font2base64 = require("node-font2base64");
 const formatMonobankData = require("../donation-tracker/formatMonobankData");
 const { imageFromBufferToBase64 } = require("../../utils/toBase64");
 
-const INSTA_STORY_TEMPLATE_HTML = "views/social-sharing/twitter.hbs";
-const COMPONENTS_CSS = "views/social-sharing/components.css";
+const FONTS_CSS = "public/styles/fonts.css";
+const COMPONENTS_CSS = "public/styles/components.css";
+const VARIABLES_CSS = "public/styles/variables.css";
+const OVERRIDES_CSS = "public/styles/overrides.css";
+const HERO_IMG = "public/images/hero.jpg";
 const TWITTER_CSS = "views/social-sharing/twitter.css";
-const VARIABLES_CSS = "views/social-sharing/variables.css";
-const OVERRIDES_CSS = "views/social-sharing/overrides.css";
-const HERO_IMG = "views/images/hero.jpg";
 
-const CSS_FILES = [VARIABLES_CSS, OVERRIDES_CSS, COMPONENTS_CSS, TWITTER_CSS];
+const TWITTER_POST_TEMPLATE_HTML = "views/social-sharing/twitter.hbs";
+
+const CSS_FILES = [FONTS_CSS, VARIABLES_CSS, OVERRIDES_CSS, COMPONENTS_CSS, TWITTER_CSS];
 
 // TELL ABOUT THIS FUNCTION WHY WE NEED IT
 const getPageTemplate = (pagePath) => fs.readFile(pagePath, { encoding: "utf-8" });
 
 const getTwitterPostParams = async () => {
-  const font = await font2base64.encodeToDataUrl("views/fonts/fonts.ttf");
+  const font = await font2base64.encodeToDataUrl("public/fonts/fonts.ttf");
   const [imageContent, ...cssPages] = await Promise.all(
     [fs.readFile(HERO_IMG)].concat(
       CSS_FILES.map((path) => fs.readFile(path, { encoding: "utf-8" }))
@@ -37,7 +39,7 @@ const getTwitterPostParams = async () => {
 async function createTwitterPostImage(jar) {
   const promises = [
     getTwitterPostParams(),
-    getPageTemplate(INSTA_STORY_TEMPLATE_HTML),
+    getPageTemplate(TWITTER_POST_TEMPLATE_HTML),
   ];
   const [{ font, css, heroImg }, page] = await Promise.all(promises);
 
